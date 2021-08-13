bb057a7002624d11c41db115083373f5acaaa87d
DaCool1 <86981743+kewlamogh@users.noreply.github.com>
2021-08-13 01:30:32 +0000

    added item class

--- machl.js
+++ machl.js
@@ -4,8 +4,8 @@ class ML {
   }
   process(attrs2) {
     let stats = {goods: 0};
-    for (var i = 0; i <= attrs2.length - 1; i ++) {
-      stats.goods += this.attrs[attrs2[i]]
+    for (var i = 0; i <= attrs2.tags.length - 1; i ++) {
+      stats.goods += this.attrs[attrs2.tags[i]]
     }
     if (stats.goods > 0) {
       return "good"
@@ -21,8 +21,18 @@ class ML {
     this.attrs[attribute] = points
   }
 }
+class Item {
+  constructor (tags, title) {
+    this.tags = tags;
+    this.title = title;
+  }
+}
 
-let machl = new ML();
-machl.train("edible", 1);
-machl.train("unhealthy", -10);
-console.log(machl.process(["edible", "unhealthy"]))
+let food_classifier = new ML();
+let hamburger = new Item(["edible", "tasty", "unhealthy"], "hamburger");
+let carrot = new Item(["edible", "tasty"], "carrot");
+food_classifier.train("edible", +1);
+food_classifier.train("tasty", +3);
+food_classifier.train("unhealthy", -10);
+console.log(food_classifier.process(hamburger));
+console.log(food_classifier.process(carrot));
